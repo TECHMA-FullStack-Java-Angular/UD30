339f5e97a0959fa17a759b20aee4998f1160c7ae
Paula Sousa <paulasousa137@gmail.com>
2023-05-02 10:53:20 +0200

    ejercicio 1.3 y ejercicio 1.4

--- ta30.1/js/codigo.js
+++ ta30.1/js/codigo.js
@@ -1,2 +1,13 @@
+/*Ejercicio 1 Javascript:
+1. Separamos el codigo javascript en un archivo externo y lo enlazamos por html. 
+Imprimimos por consola el mensaje Hola Mundo.
+2. Seguido de este mensaje imprimimos otro mensaje indicando que es el primer Script.*/
+
+//Mensaje 1
 console.log("Hola Mundo!");
+
+//Mensaje 2
 console.log("Soy el primer script");
+
+//Aviso a navegadores que no tengan soporte Javascript
+alert("Esta pagina necesita Javascript para su correcto funcionamiento.")
