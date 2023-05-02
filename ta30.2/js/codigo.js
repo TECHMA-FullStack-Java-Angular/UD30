465d1278a3625c9c54ec2a9da6f6c258db72f184
Paula Sousa <paulasousa137@gmail.com>
2023-05-02 11:02:12 +0200

    Ejercicio 2

---
+++ ta30.2/js/codigo.js
@@ -0,0 +1,16 @@
+/*Ejercicio 1 Javascript:
+1. Separamos el codigo javascript en un archivo externo y lo enlazamos por html. 
+Imprimimos por consola el mensaje Hola Mundo.
+2. Seguido de este mensaje imprimimos otro mensaje indicando que es el primer Script.*/
+
+//Mensaje 1
+console.log("Hola Mundo!");
+
+//Mensaje 2
+console.log("Soy el primer script");
+
+//Aviso a navegadores que no tengan soporte Javascript
+//alert("Esta pagina necesita Javascript para su correcto funcionamiento.")
+
+//Nuevo alerta
+let mensaje = alert("Hola Mundo!\nQué fácil es incluir 'comillas simples' y \"comillas dobles\"");
